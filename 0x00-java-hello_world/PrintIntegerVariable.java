f68d9dc8c959692f403335cbf8d1ece92b3835e9
Tcristinas19 <tcristinas19@gmail.com>
2022-05-02 14:07:02 -0700

    Sysout Int e String

---
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -0,0 +1,11 @@
+
+publi class PrintIntegerVariable {
+
+	public static void main(String[] args) {
+		int numero = 19;
+		String avenida = Avenida Andaló;
+
+		System.out.println(numero);
+		System.out.println(avenida);
+	}
+}
