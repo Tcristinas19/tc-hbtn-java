52d61869d4c72eecdfcbcf79a2cda56bb75ed43b
Tcristinas19 <tcristinas19@gmail.com>
2022-05-03 10:26:57 -0700

    String com Concatenar

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,8 @@
+public class PrintStringConcat {
+	public static void main (String[] args) {
+		String texto0 = "Bem vindo a ";
+		String texto1 = "Holberton ";
+		String texto2 = "School!";
+		System.out.println(texto0 + texto1 + texto2);
+	}
+}
