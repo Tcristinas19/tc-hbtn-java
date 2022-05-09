b4d3331c263998ec7d2751b455963383cdebfde8
Tcristinas19 <tcristinas19@gmail.com>
2022-05-09 05:17:07 -0700

    Encapsulamento Personagem

---
+++ 0x04/encapsulamento/PersonagemGame.java
@@ -0,0 +1,23 @@
+public class PersonagemGame {
+	    private String nome;
+	        private int saudeAtual;
+
+		    public PersonagemGame(){
+			        }
+
+		        public int getSaudeAtual() {
+				        return saudeAtual;
+					    }
+
+			    public void setSaudeAtual(int saudeAtual) {
+				            this.saudeAtual = saudeAtual;
+					        }
+
+			        public String getNome() {
+					        return nome;
+						    }
+
+				    public void setNome(String nome) {
+					            this.nome = nome;
+						        }
+}
