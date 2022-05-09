0d6ab43d464e1ff00acb274cc4c0b4b7245eaa63
Tcristinas19 <tcristinas19@gmail.com>
2022-05-08 18:54:19 -0700

    Salario

---
+++ 0x03/salario/Gerente.java
@@ -0,0 +1,18 @@
+public class Gerente extends Empregado{
+
+	    public Gerente(double salarioFixo){
+		            super(salarioFixo);
+			        }
+
+	        @Override
+		    public double calcularBonus(Departamento departamento){
+			            if(departamento.alcancouMeta()){
+					                super.valorBonus = (super.salarioFixo * 0.2) + ((departamento.valorAtingidoMeta - departamento.valorMeta) * 0.01);
+							        }
+				            else{
+						                super.valorBonus = 0;
+								        }
+
+					            return super.valorBonus;
+						        }
+}
