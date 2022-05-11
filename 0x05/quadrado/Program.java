716c78ea85ed62d947988afe0f25f2fe2f5fb0de
Thamires Cristina Santos <CIANDT\thamirescs@lnb035737spo.ciandt.global>
2022-05-11 14:35:39 -0300

    quadrado

--- 0x05/quadrado/Program.java
+++ 0x05/quadrado/Program.java
@@ -24,8 +24,8 @@ public class Program {
             System.out.println(ex.getMessage());
         }
 
-        System.out.printf("Lado: %2f\n", quadrado.getLado());
-        System.out.printf("Area: %2f\n", quadrado.area());
+        System.out.printf("Lado: %.2f\n", quadrado.getLado());
+        System.out.printf("Area: %.2f\n", quadrado.area());
         System.out.printf(quadrado.toString() + "\n");
     }
 }
