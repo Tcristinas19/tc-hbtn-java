d513e17682e80bbaafa933c6cb1805a9574247f2
Thamires Cristina Santos <CIANDT\thamirescs@lnb035737spo.ciandt.global>
2022-05-13 09:45:04 -0300

    Workflow

---
+++ 0x07/workflow/ProcessadorVideo.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProcessadorVideo {
+
+    private List<CanalNotificacao> canais = new ArrayList<CanalNotificacao>();
+
+    public void registrarCanal(CanalNotificacao canalNotificacao){
+        this.canais.add(canalNotificacao);
+    }
+
+    public void processar(Video video){
+        for(CanalNotificacao canal : this.canais){
+            canal.notificar(new Mensagem(video.getArquivo() + " - " + video.getFormato(), TipoMensagem.LOG));
+        }
+    }
+
+}
---
+++ 0x07/workflow/Sms.java
@@ -0,0 +1,11 @@
+public class Sms implements CanalNotificacao {
+
+    public Sms() {
+
+    }
+
+    @Override
+    public void notificar(Mensagem mensagem) {
+        System.out.println("[SMS] {" + mensagem.getTipoMensagem() + "}" + " - " + mensagem.getTexto());
+    }
+}
