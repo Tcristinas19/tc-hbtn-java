ede4bce14486e4454820b213038224d389d53f26
Thamires Cristina Santos <CIANDT\thamirescs@lnb035737spo.ciandt.global>
2022-05-30 15:26:05 -0300

    Java - Streams, Collectors e Optional

---
+++ Streams_Collectors_Optional/preco_minimo/Consulta.java
@@ -0,0 +1,29 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.*;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido){
+        return pedido.getProdutos()
+                .stream()
+                .filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO)
+                .collect(Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos){
+        List<Produto> filtered = produtos
+                .stream()
+                .sorted(Comparator.comparing(Produto::getPreco).reversed())
+                .collect(Collectors.toList());
+
+        return filtered == null ? null : filtered.get(0);
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo){
+        return produtos
+                .stream()
+                .filter(produto -> produto.getPreco() >= precoMinimo)
+                .collect(Collectors.toList());
+    }
+}
